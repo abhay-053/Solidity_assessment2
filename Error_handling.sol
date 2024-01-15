6654b4b5d571cc6d449724063b74395aa5789115
Abhay Dhek <121494832+abhay-053@users.noreply.github.com>
2024-01-15 23:30:58 +0530

    Error_handling.sol

--- Error_handling.sol
+++ Error_handling.sol
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
-
 contract ErrorHandlingContract {
-    uint public gravitationalForce = 10;
-    uint public materialDensity = 5; // New variable
+    uint public gravitationalForce = 0;
+    uint public materialDensity = 5;
     address public contractOwner;
-
     constructor() {
         contractOwner = msg.sender;
     }
     function setMaterialDensity(uint _density) public {
         require(contractOwner == msg.sender, "You are not the owner");
+        require(_density > 0, "Density must be greater than 0");
         materialDensity = _density;
     }
     function calculateWeight(uint _mass) public view returns (uint) {
@@ -20,7 +19,6 @@ contract ErrorHandlingContract {
         if (weight == 0) {
             revert("Weight of the object cannot be zero");
         }
-
         return weight;
     }
 }
